decfb99e5a1d0c6acc3a57869316b35590612b63
Francis Santiago <francis1505@gmail.com>
2024-09-12 15:33:52 -0300

    :hammer: [REFACTOR] Projeto Hello World

--- projects/hello-world/src/app/app.component.spec.ts
+++ projects/hello-world/src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'hello-world' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hello-world');
+    expect(app.title).toEqual('Hello World!');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, hello-world');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Hello World!');
   });
 });
