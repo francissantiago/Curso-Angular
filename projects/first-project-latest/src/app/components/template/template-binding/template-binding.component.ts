0e01f9972bcdcac3a0c12a15b01623a59deff3ae
Francis Santiago <francis1505@gmail.com>
2024-10-04 16:07:29 -0300

    :sparkles: Property Binding

--- projects/first-project-latest/src/app/components/template/template-binding/template-binding.component.ts
+++ projects/first-project-latest/src/app/components/template/template-binding/template-binding.component.ts
@@ -11,6 +11,8 @@ export class TemplateBindingComponent {
   public name = 'Francis Santiago';
   public age = 33;
   public test = this.age > 18 ? 'Maior' : 'Menor'
+  public isDisabled = true;
+  public srcValue = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQheGKyogVjroM8XX4446c8Mlt1ku91IMM_A&s'
 
   public sum(val1: number, val2: number){
     return val1 + val2;
