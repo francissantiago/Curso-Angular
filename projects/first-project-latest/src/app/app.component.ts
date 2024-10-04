4f1e1751acf1d3214089a470ccc3c5f859d1985f
Francis Santiago <francis1505@gmail.com>
2024-10-04 17:00:54 -0300

    :sparkles: Template Variables

--- projects/first-project-latest/src/app/app.component.ts
+++ projects/first-project-latest/src/app/app.component.ts
@@ -5,17 +5,18 @@ import { RouterOutlet } from '@angular/router';
 // Components
 import { NewComponent } from "./components/new-component/new-component.component";
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
-import { FormsModule } from '@angular/forms';
+import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent],
+  imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent, TemplateVariablesComponent],
   template: `
-  <!-- <router-outlet />
-  <app-new-component /> -->
+  <!-- <router-outlet /> -->
+  <!-- <app-new-component /> -->
+  <!-- <app-template-binding /> -->
   <h1>Curso de Angular</h1>
-  <app-template-binding />
+  <app-template-variables />
   `,
 })
 export class AppComponent {
