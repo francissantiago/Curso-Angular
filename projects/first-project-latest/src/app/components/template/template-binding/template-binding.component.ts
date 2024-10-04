bbb1bcc7b145802a195e33834c553b8b941b2953
Francis Santiago <francis1505@gmail.com>
2024-10-04 16:31:53 -0300

    :sparkles: Event binding

--- projects/first-project-latest/src/app/components/template/template-binding/template-binding.component.ts
+++ projects/first-project-latest/src/app/components/template/template-binding/template-binding.component.ts
@@ -13,9 +13,28 @@ export class TemplateBindingComponent {
   public test = this.age > 18 ? 'Maior' : 'Menor'
   public isDisabled = true;
   public srcValue = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQheGKyogVjroM8XX4446c8Mlt1ku91IMM_A&s'
-  public isTextDecoration = this.age >= 33 ? 'underline': 'none';
+  public isTextDecoration = this.age >= 33 ? 'underline' : 'none';
 
-  public sum(val1: number, val2: number){
+  public sum(val1: number, val2: number) {
     return val1 + val2;
+  }
+
+  public sumEventBinding() {
+    return this.age++;
+  }
+
+  public subEventBinding() {
+    return this.age--;
+  }
+
+  public onKeyDown(event: Event) {
+    return console.log(event);
+  }
+
+  public onMouseEvent(event: MouseEvent){
+    return console.log({
+      clientX: event.clientX,
+      clientY: event.clientY
+    });
   }
 }
