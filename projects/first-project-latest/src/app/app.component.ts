2469615a45e9dd67e063808436a65cb8e5fe6725
Francis Santiago <francis1505@gmail.com>
2024-10-09 07:42:00 -0300

    :sparkles: Introdução Deferrable Views

--- projects/first-project-latest/src/app/app.component.ts
+++ projects/first-project-latest/src/app/app.component.ts
@@ -7,6 +7,7 @@ import { NewComponent } from "./components/new-component/new-component.component
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
+import { TemplateDeferrableViewsComponent } from './components/template/template-deferrable-views/template-deferrable-views.component';
 
 @Component({
   selector: 'app-root',
@@ -17,15 +18,16 @@ import { TemplateControlFlowComponent } from './components/template/template-con
     NewComponent,
     TemplateBindingComponent,
     TemplateVariablesComponent,
-    TemplateControlFlowComponent
+    TemplateControlFlowComponent,
+    TemplateDeferrableViewsComponent
   ],
   template: `
   <!-- <router-outlet /> -->
-  <!-- <app-new-component /> -->
   <!-- <app-template-binding /> -->
   <!-- <app-template-variables /> -->
+  <!-- <app-template-control-flow /> -->
   <h1>Curso de Angular</h1>
-  <app-template-control-flow />
+  <app-template-deferrable-views />
   `,
 })
 export class AppComponent {
