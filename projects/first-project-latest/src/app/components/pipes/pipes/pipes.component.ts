b87e8d6a9da8f04b46dc651af64549b42b865b36
Francis Santiago <francis1505@gmail.com>
2024-10-22 08:30:18 -0300

    :sparkles: Pipes com Locale Global

--- projects/first-project-latest/src/app/components/pipes/pipes/pipes.component.ts
+++ projects/first-project-latest/src/app/components/pipes/pipes/pipes.component.ts
@@ -1,17 +1,15 @@
-import { CommonModule, registerLocaleData } from '@angular/common';
-import { Component, LOCALE_ID, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 import { delay, Observable, of } from 'rxjs';
 
-import localePt from '@angular/common/locales/pt';
-registerLocaleData(localePt);
+
 
 @Component({
   selector: 'app-pipes',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './pipes.component.html',
-  styleUrl: './pipes.component.scss',
-  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
+  styleUrl: './pipes.component.scss'
 })
 export class PipesComponent {
   public date = signal(new Date());
