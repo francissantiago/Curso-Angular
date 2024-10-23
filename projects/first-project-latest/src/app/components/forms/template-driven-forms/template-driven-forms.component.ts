36cfa71935914e1b3299b79b83a94f83b2076e90
Francis Santiago <francis1505@gmail.com>
2024-10-23 14:02:57 -0300

    :sparkles: Form Select

--- projects/first-project-latest/src/app/components/forms/template-driven-forms/template-driven-forms.component.ts
+++ projects/first-project-latest/src/app/components/forms/template-driven-forms/template-driven-forms.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven-forms',
   standalone: true,
-  imports: [FormsModule],
+  imports: [JsonPipe, FormsModule],
   templateUrl: './template-driven-forms.component.html',
   styleUrl: './template-driven-forms.component.scss'
 })
 export class TemplateDrivenFormsComponent {
-
+  public listComidas = signal<Array<{ comida: string, preco: string }>>([
+    { comida: 'X-salada', preco: 'R$ 10' },
+    { comida: 'X-bacon', preco: 'R$ 11' },
+    { comida: 'Coxinha', preco: 'R$ 6' },
+  ]);
 }
