471947521878b63a9c0f775295b651400e91c2bd
Francis Santiago <francis1505@gmail.com>
2024-10-23 16:10:09 -0300

    :sparkles: Custom Validators

--- projects/first-project-latest/src/app/components/forms/reactive-forms/reactive-forms.component.ts
+++ projects/first-project-latest/src/app/components/forms/reactive-forms/reactive-forms.component.ts
@@ -1,6 +1,19 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+
+function textValidator(): ValidatorFn {
+  return (control: AbstractControl) => {
+    const hasUpperCase = /[A-Z]/.test(control.value);
+    const hasNumber = /[1-9]/.test(control.value);
+
+    if (hasUpperCase && hasNumber) {
+      return null;
+    }
+
+    return { textValidator: true };
+  }
+}
 
 @Component({
   selector: 'app-reactive-forms',
@@ -18,7 +31,7 @@ export class ReactiveFormsComponent {
   #fb = inject(FormBuilder);
 
   public profileForm = this.#fb.group({
-    name: ['', [Validators.minLength(3), Validators.maxLength(10), Validators.required]],
+    name: ['', [Validators.minLength(3), Validators.maxLength(10), Validators.required, textValidator()]],
     myStacks: this.#fb.group({
       front: ['Angular'],
       back: ['PHP']
